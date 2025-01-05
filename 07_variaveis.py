72445486ac0b55fadf0b2f49f3f2d3b7bf517fe9
Henrique Andrade <henrique.portugal15@gmail.com>
2025-01-05 16:32:07 +0000

    Add files via upload

---
+++ 07_variaveis.py
@@ -0,0 +1,10 @@
+#variáveis
+
+nome_completo = "Henrique Andrade"
+print(nome_completo)
+
+nome = "Henrique"
+idade = 17
+maior_de_idade = idade >= 18
+print("nome:", nome, "idade", idade) #string "nome:" / valor de (nome,), que é Henrique
+print("É maior?", maior_de_idade)
